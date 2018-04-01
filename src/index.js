9ffbf334bfac8b327ecaac38c9d9bfdfd59d7d7d
lexloo@126.com <lexloo@126.com>
2018-04-01 20:45:17 +0800

    to 2.14

--- src/index.js
+++ src/index.js
@@ -48,120 +48,141 @@ import Tooltip from './components/tooltip';
 import Transfer from './components/transfer';
 import Tree from './components/tree';
 import Upload from './components/upload';
-import {Row, Col} from './components/grid';
-import {Select, Option, OptionGroup} from './components/select';
+import {
+	Row,
+	Col
+} from './components/grid';
+import {
+	Select,
+	Option,
+	OptionGroup
+} from './components/select';
 import locale from './locale/index';
 
 const components = {
-    Affix,
-    Alert,
-    AutoComplete,
-    Avatar,
-    BackTop,
-    Badge,
-    Breadcrumb,
-    BreadcrumbItem: Breadcrumb.Item,
-    Button,
-    ButtonGroup: Button.Group,
-    Card,
-    Carousel,
-    CarouselItem: Carousel.Item,
-    Cascader,
-    Checkbox,
-    CheckboxGroup: Checkbox.Group,
-    Col,
-    Collapse,
-    ColorPicker,
-    Content: Content,
-    DatePicker,
-    Dropdown,
-    DropdownItem: Dropdown.Item,
-    DropdownMenu: Dropdown.Menu,
-    Footer: Footer,
-    Form,
-    FormItem: Form.Item,
-    Header: Header,
-    Icon,
-    Input,
-    InputNumber,
-    Scroll,
-    Sider: Sider,
-    Submenu: Menu.Sub,
-    Layout: Layout,
-    LoadingBar,
-    Menu,
-    MenuGroup: Menu.Group,
-    MenuItem: Menu.Item,
-    Message,
-    Modal,
-    Notice,
-    Option: Option,
-    OptionGroup,
-    Page,
-    Panel: Collapse.Panel,
-    Poptip,
-    Progress,
-    Radio,
-    RadioGroup: Radio.Group,
-    Rate,
-    Row,
-    Select,
-    Slider,
-    Spin,
-    Step: Steps.Step,
-    Steps,
-    Table,
-    Tabs: Tabs,
-    TabPane: Tabs.Pane,
-    Tag,
-    Timeline,
-    TimelineItem: Timeline.Item,
-    TimePicker,
-    Tooltip,
-    Transfer,
-    Tree,
-    Upload
+	Affix,
+	Alert,
+	AutoComplete,
+	Avatar,
+	BackTop,
+	Badge,
+	Breadcrumb,
+	BreadcrumbItem: Breadcrumb.Item,
+	Button,
+	ButtonGroup: Button.Group,
+	Card,
+	Carousel,
+	CarouselItem: Carousel.Item,
+	Cascader,
+	Checkbox,
+	CheckboxGroup: Checkbox.Group,
+	Col,
+	Collapse,
+	ColorPicker,
+	Content: Content,
+	DatePicker,
+	Dropdown,
+	DropdownItem: Dropdown.Item,
+	DropdownMenu: Dropdown.Menu,
+	Footer: Footer,
+	Form,
+	FormItem: Form.Item,
+	Header: Header,
+	Icon,
+	Input,
+	InputNumber,
+	Scroll,
+	Sider: Sider,
+	Submenu: Menu.Sub,
+	Layout: Layout,
+	LoadingBar,
+	Menu,
+	MenuGroup: Menu.Group,
+	MenuItem: Menu.Item,
+	Message,
+	Modal,
+	Notice,
+	Option: Option,
+	OptionGroup,
+	Page,
+	Panel: Collapse.Panel,
+	Poptip,
+	Progress,
+	Radio,
+	RadioGroup: Radio.Group,
+	Rate,
+	Row,
+	Select,
+	Slider,
+	Spin,
+	Step: Steps.Step,
+	Steps,
+	Table,
+	Tabs: Tabs,
+	TabPane: Tabs.Pane,
+	Tag,
+	Timeline,
+	TimelineItem: Timeline.Item,
+	TimePicker,
+	Tooltip,
+	Transfer,
+	Tree,
+	Upload
 };
 
-const UIWeb = {
-    ...components
+const iview = {
+	...components,
+	iButton: Button,
+	iCircle: Circle,
+	iCol: Col,
+	iContent: Content,
+	iForm: Form,
+	iFooter: Footer,
+	iHeader: Header,
+	iInput: Input,
+	iMenu: Menu,
+	iOption: Option,
+	iProgress: Progress,
+	iSelect: Select,
+	iSwitch: Switch,
+	iTable: Table
 };
 
 const install = function(Vue, opts = {}) {
-    if (install.installed) return;
-    locale.use(opts.locale);
-    locale.i18n(opts.i18n);
+	if (install.installed) return;
+	locale.use(opts.locale);
+	locale.i18n(opts.i18n);
 
-    Object.keys(UIWeb).forEach(key => {
-        Vue.component('ui-' + key, UIWeb[key]);
-    });
+	Object.keys(iview).forEach(key => {
+		Vue.component(key, iview[key]);
+	});
 
-    Vue.prototype.$Loading = LoadingBar;
-    Vue.prototype.$Message = Message;
-    Vue.prototype.$Modal = Modal;
-    Vue.prototype.$Notice = Notice;
-    Vue.prototype.$Spin = Spin;
+	Vue.prototype.$Loading = LoadingBar;
+	Vue.prototype.$Message = Message;
+	Vue.prototype.$Modal = Modal;
+	Vue.prototype.$Notice = Notice;
+	Vue.prototype.$Spin = Spin;
 };
 
 // auto install
 if (typeof window !== 'undefined' && window.Vue) {
-    install(window.Vue);
+	install(window.Vue);
 }
 
 const API = {
-    version: process.env.VERSION, // eslint-disable-line no-undef
-    locale: locale.use,
-    i18n: locale.i18n,
-    install,
-    Circle,
-    Switch,
-    ...components
+	version: process.env.VERSION, // eslint-disable-line no-undef
+	locale: locale.use,
+	i18n: locale.i18n,
+	install,
+	Circle,
+	Switch,
+	...components
 };
 
 API.lang = (code) => {
-    const langObject = window['UIWeb/locale'].default;
-    if (code === langObject.i.locale) locale.use(langObject);
-    else console.log(`The ${code} language pack is not loaded.`); // eslint-disable-line no-console
+	const langObject = window['iview/locale'].default;
+	if (code === langObject.i.locale) locale.use(langObject);
+	else console.log(`The ${code} language pack is not loaded.`); // eslint-disable-line no-console
 };
 
-module.exports.default = module.exports = API;   // eslint-disable-line no-undef
+module.exports.default = module.exports = API; // eslint-disable-line no-undef
